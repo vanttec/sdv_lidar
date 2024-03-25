0ed50b902624162fd42d40ee6e9b11c5820f9661
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2024-03-25 15:56:21 -0600

    sync ended

--- lidar_imu_sync/src/LidarImuSync.cpp
+++ lidar_imu_sync/src/LidarImuSync.cpp
@@ -3,12 +3,14 @@
 
 // Ros2
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+
 
 // PCL
 #include <pcl/point_types.h>
@@ -36,7 +38,30 @@ class LidarImuSync : public rclcpp::Node
 {
 private:
     /* data */
-    using SyncPolicy = message_filters::sync_policies::ApproximateTime<PointCloudMsg, PointCloudMsg>;
+    using PointCloudMsg = sensor_msgs::msg::PointCloud2;
+    using ImuMsg = sensor_msgs::msg::Imu;   
+
+    using SyncPolicy = message_filters::sync_policies::ApproximateTime<ImuMsg, PointCloudMsg>;
+
+    // main callback function
+    void points_imuCallback(const ImuMsg::ConstSharedPtr &msg1, const PointCloudMsg::ConstSharedPtr &msg2);
+                            
+    // message filter synchronizer object
+    std::unique_ptr<message_filters::Synchronizer<SyncPolicy>>
+        point_cloud_imu_synchronizer_;
+
+    // final fused point cloud publisher object
+    rclcpp::Publisher<PointCloudMsg>::SharedPtr fused_point_cloud_imu_publisher_;
+    // final imu publisher
+    rclcpp::Publisher<ImuMsg>::SharedPtr fused_imu_publisher_;
+
+    // need two subscribers for lidar and imu
+    std::unique_ptr<message_filters::Subscriber<PointCloudMsg>>
+        lidar_subscriber_;
+    std::unique_ptr<message_filters::Subscriber<ImuMsg>>
+        imu_subscriber_;
+
+
 public:
     LidarImuSync(/* args */);
     ~LidarImuSync();
@@ -44,10 +69,58 @@ public:
 
 LidarImuSync::LidarImuSync(/* args */) : Node("lidar_imu_sync")
 {
+
+    // tf2 buffer
+    tf2_ros::Buffer tf2_buffer(this->get_clock());
+    // tf2 listener
+    tf2_ros::TransformListener tf2_listener(tf2_buffer);
+
+    fused_point_cloud_imu_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/points_fusion", 10);
+    fused_imu_publisher_ = this->create_publisher<sensor_msgs::msg::Imu>("/imu_fusion", 10);
+
+  // initialize front and rear lidar subscriber objects
+  lidar_subscriber_ =
+      std::make_unique<message_filters::Subscriber<PointCloudMsg>>(this,
+                                                                   "/velodyne_points");
+  imu_subscriber_ =
+      std::make_unique<message_filters::Subscriber<ImuMsg>>(this,
+                                                                "/vectornav/imu");
+
+  // initialize message filter stuffs
+  point_cloud_imu_synchronizer_ =
+      std::make_unique<message_filters::Synchronizer<SyncPolicy>>(
+          SyncPolicy(10), *imu_subscriber_, *lidar_subscriber_);
+
+  point_cloud_imu_synchronizer_->registerCallback(
+      std::bind(&LidarImuSync::points_imuCallback, this,
+                std::placeholders::_1, std::placeholders::_2));
+
+
+    RCLCPP_INFO(this->get_logger(), "\033[1;32m----> lidar_imu_sync_node initialized.\033[0m");
+
+    
+
+
+
 }
 
 LidarImuSync::~LidarImuSync()
 {
+}
+
+void LidarImuSync::points_imuCallback(const ImuMsg::ConstSharedPtr &msg1, const PointCloudMsg::ConstSharedPtr &msg2)
+{
+    RCLCPP_INFO(this->get_logger(), "\033[1;34m -> lidar & imu data received <- \033[0m");
+
+    // get the imu data
+    sensor_msgs::msg::Imu imu_data = *msg1;
+    // get the point cloud data
+    sensor_msgs::msg::PointCloud2 point_cloud_data = *msg2;
+
+    // publish the imu data
+    fused_imu_publisher_->publish(imu_data);
+    // publish the point cloud data
+    fused_point_cloud_imu_publisher_->publish(point_cloud_data);
 }
 
 int main(int argc, char** argv) {
